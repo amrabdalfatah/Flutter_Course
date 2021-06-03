56bc7a9525a0671ea970d8726572ca268ee288d3
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-06-03 13:13:14 +0200

    SafeArea when no AppBar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,31 +4,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          Icons.menu,
-        ),
-        title: Text(
-          'First App',
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.notification_important,
-            ),
-            onPressed: () {
-              print('Icon Notification Clicked');
-            },
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.search,
-            ),
-            onPressed: () {
-              print('Search is clicked');
-            },
-          ),
-        ],
+      // appBar: AppBar(
+      //   backgroundColor: Colors.red,
+      //   leading: Icon(
+      //     Icons.menu,
+      //   ),
+      //   title: Text(
+      //     'First App',
+      //   ),
+      //   actions: [
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.notification_important,
+      //       ),
+      //       onPressed: () {
+      //         print('Icon Notification Clicked');
+      //       },
+      //     ),
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.search,
+      //       ),
+      //       onPressed: () {
+      //         print('Search is clicked');
+      //       },
+      //     ),
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Text('Hello Flutter'),
       ),
     );
   }
