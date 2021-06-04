1e3b3c3049d33ec82c98c84a1b10aa484d754fbf
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-06-04 02:02:30 +0200

    Column and Expanded

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,35 +4,84 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.red,
-      //   leading: Icon(
-      //     Icons.menu,
-      //   ),
-      //   title: Text(
-      //     'First App',
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //       icon: Icon(
-      //         Icons.notification_important,
-      //       ),
-      //       onPressed: () {
-      //         print('Icon Notification Clicked');
-      //       },
-      //     ),
-      //     IconButton(
-      //       icon: Icon(
-      //         Icons.search,
-      //       ),
-      //       onPressed: () {
-      //         print('Search is clicked');
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body: SafeArea(
-        child: Text('Hello Flutter'),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        leading: Icon(
+          Icons.menu,
+        ),
+        title: Text(
+          'First App',
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.notification_important,
+            ),
+            onPressed: () {
+              print('Icon Notification Clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+            onPressed: () {
+              print('Search is clicked');
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.red,
+              child: Text(
+                'First Text',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: Text(
+                'Second Text',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Text(
+                'Third Text',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: Text(
+                'Fourth Text',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
