7ad519ed701e5a6da3c908b19b10e16a34146264
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-06-07 19:40:55 +0200

    Finish the ListView with class Model

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/material.dart';
 
+class UserModel {
+  final int id;
+  final String name;
+  final String phone;
+
+  UserModel({
+    @required this.id,
+    @required this.name,
+    @required this.phone,
+  });
+}
+
 class UsersScreen extends StatelessWidget {
   // 1. Build Item
   // 2. Build List
   // 3. add item to list
+
+  final List<UserModel> users = [
+    UserModel(
+      id: 1,
+      name: 'Abdalfatah Mohamed',
+      phone: '+201016716496',
+    ),
+    UserModel(
+      id: 2,
+      name: 'Ahmed Ali',
+      phone: '+201016716496',
+    ),
+    UserModel(
+      id: 3,
+      name: 'Mohamed Abdalfatah',
+      phone: '+201016716496',
+    ),
+    UserModel(
+      id: 4,
+      name: 'Amr Abdalfatah',
+      phone: '+201016716496',
+    ),
+    UserModel(
+      id: 5,
+      name: 'Ali Abdalfatah',
+      phone: '+201016716496',
+    ),
+    UserModel(
+      id: 6,
+      name: 'Adel Abdalfatah',
+      phone: '+201016716496',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +58,27 @@ class UsersScreen extends StatelessWidget {
           'Users',
         ),
       ),
-      body: Padding(
+      body: ListView.separated(
+        itemBuilder: (context, index) => buildUserItem(users[index]),
+        separatorBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Divider(
+            color: Colors.grey[500],
+          ),
+        ),
+        itemCount: users.length,
+      ),
+    );
+  }
+
+  Widget buildUserItem(UserModel user) => Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
             CircleAvatar(
               radius: 25.0,
               child: Text(
-                '1',
+                user.id.toString(),
                 style: TextStyle(
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
@@ -36,14 +94,14 @@ class UsersScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Text(
-                    'Amr Abdalfatah',
+                    user.name,
                     style: TextStyle(
                       fontSize: 25.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
-                    '01016716496',
+                    user.phone,
                     style: TextStyle(
                       color: Colors.grey,
                       fontSize: 18.0,
@@ -54,7 +112,5 @@ class UsersScreen extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
+      );
 }
